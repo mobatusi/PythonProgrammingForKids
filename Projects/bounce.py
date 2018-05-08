f8d15ccef448610b529774f453f1420f12d58b10
Dolu Obatusin <mobatusin.tcsec@gmail.com>
2018-05-08 05:00:01 -0400

    added main window page

--- Projects/bounce.py
+++ Projects/bounce.py
@@ -3,3 +3,19 @@ This is a game with a bouncing ball and a paddle. The ball will fly around the s
 off the paddle. If the ball hits the bottom of the screen, the game comes to an end.
 
 """
+
+# Creating the game canvas
+
+from Tkinter import *
+import random
+import time
+
+tk = Tk()
+tk.title("Game")    # window title
+tk.resizable(0,0)   # size of the window cannot be changed either horizontally or vertically
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width =500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+tk.mainloop()       # Keeps graphics window open
