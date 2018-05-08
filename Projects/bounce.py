8fa532acbff48632e96fe73a2e74cde3d7601aad
Dolu Obatusin <mobatusin.tcsec@gmail.com>
2018-05-08 04:44:36 -0400

    working on new game; bonce

---
+++ Projects/bounce.py
@@ -0,0 +1,5 @@
+"""
+This is a game with a bouncing ball and a paddle. The ball will fly around the screen, and the player will bounce it
+off the paddle. If the ball hits the bottom of the screen, the game comes to an end.
+
+"""
